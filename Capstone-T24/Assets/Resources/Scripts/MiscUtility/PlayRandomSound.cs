580023a20d6822e923f1a4c875e81974a1d9a2c1
Cody Jackson <cody@krselectric.com>
2022-11-26 14:15:56 -0800

    continue refining Lab1
    add wall/ceiling texture, fix red button light up texture, add specific audio clip time function + space out when some sounds play, add ExitTime to doors which fixes a problem and makes a new one.

--- Capstone-T24/Assets/Resources/Scripts/MiscUtility/PlayRandomSound.cs
+++ Capstone-T24/Assets/Resources/Scripts/MiscUtility/PlayRandomSound.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,11 @@ using UnityEngine;
 public class PlayRandomSound : MonoBehaviour
 {
     public List<AudioClip> SoundClips = new List<AudioClip>();
+    public AudioClip specificSound = null;
     public AudioSource source;
+
+    private bool doOnce = false;
+    private bool doOnce2 = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +27,42 @@ public class PlayRandomSound : MonoBehaviour
     public void PlayClipRandom()
     {
         int len = SoundClips.Count - 1; // because 0 is included
-        int random = Random.Range(0, len);
+        int random = UnityEngine.Random.Range(0, len);
 
         source.PlayOneShot(SoundClips[random]);
     }
+
+    public void DoTimedRandom(float time)
+    {
+        if (!doOnce)
+        {
+            StartCoroutine(TimedRandom(time));
+        }
+        doOnce = true;
+    }
+
+    IEnumerator TimedRandom(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        PlayClipRandom();
+    }
+
+    public void DoTimedSpecific(float time)
+    {
+        if (!doOnce2)
+        {
+            StartCoroutine(TimedRandom(time));
+        }
+        doOnce2 = true;
+    }
+
+    IEnumerator TimedSpecific(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        source.PlayOneShot(specificSound);
+    }
+
+
 }
